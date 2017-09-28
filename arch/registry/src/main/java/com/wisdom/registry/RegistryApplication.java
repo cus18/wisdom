c994fce84b35be307825adeda95c7390442a9388
cjk18 <cus18@sina.com>
2017-09-28 10:17:52 +0800

    springcloud config

--- arch/registry/src/main/java/com/wisdom/registry/RegistryApplication.java
+++ arch/registry/src/main/java/com/wisdom/registry/RegistryApplication.java
@@ -1,15 +1,26 @@
 package com.wisdom.registry;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaServer
+@RestController
+@RefreshScope
 public class RegistryApplication {
+
+	@Value("${env.name:World!}") String name ;
+
+	@RequestMapping("/app")
+	public String home(){
+		return "Hello " + name;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
+
 }
