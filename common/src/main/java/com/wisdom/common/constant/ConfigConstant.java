be975a755b705793fa08b9689d4a738c37567b9e
chenjiake <cus18@sina.com>
2017-11-01 16:47:08 +0800

    wisdom

--- common/src/main/java/com/wisdom/common/constant/ConfigConstant.java
+++ common/src/main/java/com/wisdom/common/constant/ConfigConstant.java
@@ -18,4 +18,30 @@ public enum ConfigConstant {
 
     public static final String EASEMOB_MESSAGE_URL = Global.getConfig("easemob.message.url");
 
+	public static final String AMR_TOMP3_FUNC = Global.getConfig("AMR_TOMP3_FUNC");
+
+	public static final String AMR_TOMP3_WINDOWSPATHTEMP = Global.getConfig("AMR_TOMP3_WINDOWSPATHTEMP");
+
+	public static final String AMR_TOMP3_WINDOWSPATH = Global.getConfig("AMR_TOMP3_WINDOWSPATH");
+
+	public static final String AMR_TOMP3_LINUXPATH = Global.getConfig("AMR_TOMP3_LINUXPATH");
+
+	public static final String CORPID = Global.getConfig("CORPID");
+
+	public static final String ANGEL_WEB_URL = Global.getConfig("ANGEL_WEB_URL");
+
+	public static final String TITAN_WEB_URL = Global.getConfig("TITAN_WEB_URL");
+
+	public static final String WISDOM_WEB_URL = Global.getConfig("WISDOM_WEB_URL");
+
+	public static final String KEEPER_WEB_URL = Global.getConfig("KEEPER_WEB_URL");
+
+	public static final String MARKET_WEB_URL = Global.getConfig("MARKET_WEB_URL");
+
+	public static final String SECTET = Global.getConfig("SECTET");
+
+	public static final String DOMAIN_VALUE = Global.getConfig("DOMAIN_VALUE");
+
+	public static final String PARTNER_KEY = Global.getConfig("PARTNER_KEY");
+
 }
