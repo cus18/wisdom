cb85aa1b883e215b48ae2c6976c50a796567b388
chenjiake <cus18@sina.com>
2017-12-05 21:23:32 +0800

    wisdom

--- service/living-service/src/main/java/com/wisdom/living/client/CoreServiceClient.java
+++ service/living-service/src/main/java/com/wisdom/living/client/CoreServiceClient.java
@@ -1,6 +1,5 @@
 package com.wisdom.living.client;
 
-import com.wisdom.common.dto.basic.BannerDTO;
 import com.wisdom.common.dto.core.DictDTO;
 import com.wisdom.common.dto.core.Page;
 import com.wisdom.common.dto.core.ResponseDTO;
@@ -9,11 +8,11 @@ import com.wisdom.common.dto.core.user.ElderUserDTO;
 import com.wisdom.common.dto.core.user.RelativeElderDTO;
 import com.wisdom.common.dto.core.user.UserInfoDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @FeignClient("core-service")
@@ -31,8 +30,8 @@ public interface CoreServiceClient {
     @RequestMapping(value = "/getEasemobGroup",method=RequestMethod.GET)
     EasemobGroupDTO getEasemobGroup(String elderId);
 
-    @RequestMapping(value = "/getUserInfoValue",method=RequestMethod.GET)
-    ResponseDTO<UserInfoDTO> getUserInfo(@RequestParam(value = "loginToken") String loginToken);
+    @RequestMapping(value = "/getUserInfo",method=RequestMethod.GET)
+    UserInfoDTO getUserInfo(HttpServletRequest request);
 
     @RequestMapping(value = "/getElderUserByHospitalID",method=RequestMethod.GET)
     Page<ElderUserDTO> getElderUserByHospitalID(@RequestParam(value = "id") String id,
@@ -41,10 +40,7 @@ public interface CoreServiceClient {
     @RequestMapping(value = "/getEasemobGroupIDByElderID",method=RequestMethod.GET)
     String getEasemobGroupIDByElderID(String elderId);
 
-    @RequestMapping(value = "/findDictListByInfo",method=RequestMethod.POST)
-    ResponseDTO<List<DictDTO>> findDictListByInfo(@RequestBody DictDTO dictDTO);
-
-    @RequestMapping(value = "/bannerList", method = {RequestMethod.GET})
-    ResponseDTO<List<BannerDTO>> getBannerList();
+    @RequestMapping(value = "/getEasemobGroupIDByElderID",method=RequestMethod.GET)
+    List<DictDTO> findDictListByInfo(DictDTO dictDTO);
 
 }
