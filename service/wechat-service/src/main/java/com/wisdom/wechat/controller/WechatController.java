19f9fe47e98428a912cbd90d4b4cb42f2f2702f7
sunxiao <897909806@qq.com>
2018-03-06 14:11:26 +0800

    提交测试

--- service/wechat-service/src/main/java/com/wisdom/wechat/controller/WechatController.java
+++ service/wechat-service/src/main/java/com/wisdom/wechat/controller/WechatController.java
@@ -93,7 +93,7 @@ public class WechatController {
 	}
 
 	/**
-	 * 获取微信 Token 的信息
+	 * 获取微信 Token 信息
 	 * @return
 	 */
 	@RequestMapping(value = "getWeChatToken", method = {RequestMethod.POST, RequestMethod.GET})
